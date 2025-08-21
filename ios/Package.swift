2fc3597b6d03b77716a988ff1c36a8ecdbc55bfd
Paul Salisbury <paul@blockchainlabs.nz>
2025-08-22 10:40:08 +1200

    chore: cleanup package.swift

--- ios/Package.swift
+++ ios/Package.swift
@@ -2,15 +2,26 @@
 import PackageDescription
 
 let package = Package(
-    name: "KeystorePlugin",
+    name: "tauri-plugin-keystore",
     platforms: [
         .iOS(.v17)
     ],
     products: [
-        .library(name: "KeystorePlugin", targets: ["KeystorePlugin"])
+        .library(
+            name: "tauri-plugin-keystore",
+            type: .static,
+            targets: ["tauri-plugin-keystore"]),
     ],
     targets: [
-        .target(name: "KeystorePlugin", path: "Sources/KeystorePlugin"),
-        .testTarget(name: "KeystorePluginTests", dependencies: ["KeystorePlugin"], path: "Tests/KeystorePluginTests")
+        .target(
+            name: "tauri-plugin-keystore",
+            dependencies: [
+                .byName(name: "Tauri")
+            ],
+            path: "Sources/KeystorePlugin"),
+        .testTarget(
+            name: "KeystorePluginTests",
+            dependencies: ["tauri-plugin-keystore"],
+            path: "Tests/KeystorePluginTests")
     ]
 )
